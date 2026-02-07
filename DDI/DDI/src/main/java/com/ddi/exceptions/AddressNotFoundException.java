b0b4a531efbd199bbdd5aae557bfd77854f7cc03
“Programador753” <antoniohcavero@outlook.com>
2026-02-07 23:13:36 +0100

    actualizo

--- DDI/DDI/src/main/java/com/ddi/exceptions/AddressNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ddi.exceptions;
-
-public class AddressNotFoundException extends Exception {
-    public AddressNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- DDI/DDI/src/main/java/com/ddi/exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ddi.exceptions;
-
-public class EmployeeNotFoundException extends Exception {
-    public EmployeeNotFoundException(String msg) {
-        super(msg);
-    }
-}
